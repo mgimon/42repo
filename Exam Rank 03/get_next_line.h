65d4d7d6328fa619b1488824265bfc3a4880b40c
Manuel Gimon Caballero <mgimon-c@cbr15s2.42barcelona.com>
2024-10-02 10:59:32 +0200

    02102024

---
+++ Exam Rank 03/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirifern <mirifern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 21:30:23 by mirifern          #+#    #+#             */
+/*   Updated: 2024/07/04 21:35:48 by mirifern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
